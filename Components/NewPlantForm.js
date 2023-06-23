3c0f70c678526723c3c850bc0ad08b9ef946465d
Alyssa <alyssa@kvasl.com>
2023-06-23 21:44:41 +1200

    Created new plant form

---
+++ Components/NewPlantForm.js
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+//Image is placeholder for now
+
+const NewPlantForm = () => {
+  return (
+    <View>
+      <Text>New Plant Type</Text>
+      <Image />
+      <View>
+        <Text>name:</Text>
+      </View>
+      <View>
+        <Text>status effects:</Text>
+      </View>
+      <View>
+        <Text>life cycle:</Text>
+      </View>
+    </View>
+  )
+}
+
+export default NewPlantForm
+
+const styles = StyleSheet.create({})
