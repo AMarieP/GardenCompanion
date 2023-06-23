49e63f0e36a177913a6c89b297871e13d13c8dda
Alyssa <alyssa@kvasl.com>
2023-06-23 14:29:10 +1200

    Created The Default Colours File

---
+++ colours.js
@@ -0,0 +1,12 @@
+export default {
+    green: "#7F8B59",
+    purple: "#6C496A",
+    mustard: "#EDBB3F",
+    navy: "#282488",
+    red: "#D04E3E",
+    greenLight: "#CFD9B1",
+    purpleLight: "#897888",
+    mustardLight: "#E4CE98",
+    navyLight: "#6E6C91",
+    redLight: "#D79A93",
+  };
