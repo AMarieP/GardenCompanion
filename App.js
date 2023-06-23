98899e896d97b66cef57c3ee377e9ff308c5d8bc
Alyssa <alyssa@kvasl.com>
2023-06-23 14:43:23 +1200

    Added Ovo font to app w loading

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Controls my font
+import AppLoading from 'expo-app-loading';
+import { useFonts, Ovo_400Regular } from '@expo-google-fonts/ovo';
+
 import PlantCard from './Components/PlantCard';
 import testPlant from './testPlant';
 
-
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
+  
+  let [fontsLoaded] = useFonts({
+    Ovo_400Regular,
+  });
+
+  let fontSize = 24;
+  let paddingVertical = 6;
+  
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <View style={styles.container}>
+      <Text style={{fontFamily: 'Ovo_400Regular'}} >Hello</Text>
       <View style={{flexDirection: 'row', width: '100%', justifyContent: 'space-around'}}>
       <PlantCard props={testPlant}/>
       <PlantCard props={testPlant}/>
       </View>
     </View>
-  );
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
