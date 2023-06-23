96af133f4757d7def0c4952d6d4708809412e995
Alyssa <alyssa@kvasl.com>
2023-06-23 22:19:30 +1200

    React Native Hook Form installed

--- App.js
+++ App.js
@@ -5,8 +5,20 @@ import { StyleSheet, Text, View } from 'react-native';
 import AppLoading from 'expo-app-loading';
 import { useFonts, Ovo_400Regular } from '@expo-google-fonts/ovo';
 
+//Imports
+
 import PlantCard from './Components/PlantCard';
 import testPlant from './testPlant';
+
+//Form
+import NewPlantForm from './Components/NewPlantForm';
+
+//buttonSVG
+import { CustomStatus } from './Components/SVGIcons/CustomStatus';
+import { FertEffect } from './Components/SVGIcons/FertEffect';
+import { WaterEffect } from './Components/SVGIcons/WaterEffect';
+import { EditButton } from './Components/SVGIcons/EditButton';
+import { AddButton } from './Components/SVGIcons/AddButton';
 
 export default function App() {
   
@@ -14,20 +26,19 @@ export default function App() {
     Ovo_400Regular,
   });
 
-  let fontSize = 24;
-  let paddingVertical = 6;
   
   if (!fontsLoaded) {
     return <AppLoading />;
   } else {
     return (
       <View style={styles.container}>
-      <Text style={{fontFamily: 'Ovo_400Regular'}} >Hello</Text>
+      {/* <Text style={{fontFamily: 'Ovo_400Regular'}} >Hello</Text>
       <View style={{flexDirection: 'row', width: '100%', justifyContent: 'space-around'}}>
       <PlantCard props={testPlant}/>
       <PlantCard props={testPlant}/>
+      </View> */}
+        <NewPlantForm />
       </View>
-    </View>
     );
   }
 
--- Components/NewPlantForm.js
+++ Components/NewPlantForm.js
@@ -1,23 +1,50 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native'
+import { useForm, Controller } from "react-hook-form";
 import React from 'react'
 
 //Image is placeholder for now
 
 const NewPlantForm = () => {
-  return (
-    <View>
-      <Text>New Plant Type</Text>
-      <Image />
-      <View>
-        <Text>name:</Text>
-      </View>
-      <View>
-        <Text>status effects:</Text>
-      </View>
-      <View>
-        <Text>life cycle:</Text>
-      </View>
-    </View>
+    const {
+        control,
+        handleSubmit,
+        formState: { errors },
+      } = useForm({})
+    
+     const onSubmit = (data) => console.log(data)
+
+    return (
+        <View>
+        <Text>New Plant Type</Text>
+        <Image />
+        <View>
+            <Text>name:</Text>
+            <Controller
+        control={control}
+        rules={{
+          required: true,
+          maxLength: 5
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            placeholder='e.g. "beefsteak tomato" or "golden pothos"'
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="plantName"
+      />
+      {errors.firstName && <Text>This is required.</Text>}
+
+        </View>
+        <View>
+            <Text>status effects:</Text>
+        </View>
+        <View>
+            <Text>life cycle:</Text>
+        </View>
+        </View>
   )
 }
 
