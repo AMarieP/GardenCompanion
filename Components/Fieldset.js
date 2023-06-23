14e38ab3d8098c0198c91b44cdb96d404d23c2cc
Alyssa <alyssa@kvasl.com>
2023-06-24 01:02:05 +1200

    Fieldset and new plant form looking semi ok

---
+++ Components/Fieldset.js
@@ -0,0 +1,34 @@
+import { StyleSheet, Text, View } from 'react-native'
+import H2 from './H2'
+import MyText from './MyText'
+import React from 'react'
+
+const Fieldset = (props) => {
+  return (
+    <View  {...props} style={styles.fieldSet}>
+        <H2 style={styles.legend}>{props.title}</H2>
+        <MyText>{props.children}</MyText>
+    </View>
+  )
+}
+
+export default Fieldset
+
+const styles = StyleSheet.create({
+    fieldSet:{
+        marginVertical: 10,
+        paddingVertical: 30,
+        borderWidth: 1,
+        alignItems: 'center',
+        borderColor: '#000',
+        zIndex: -1,
+        minHeight: 100
+    },
+    legend:{
+        position: 'absolute',
+        top: -10,
+        left: 10,
+        backgroundColor: 'white',
+        fontSize: 36,
+    }
+})
