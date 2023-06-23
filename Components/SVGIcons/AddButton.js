64a3182616436ff4cd2d1bc989dfe532f53489e3
Alyssa <alyssa@kvasl.com>
2023-06-23 21:15:31 +1200

    Added svgs in folder, made svg icons scalable

--- Components/SVGIcons/AddButton.js
+++ Components/SVGIcons/AddButton.js
@@ -1,20 +1,25 @@
 import * as React from "react"
+import { View } from 'react-native'
 import Svg, { Circle, Path } from "react-native-svg"
-const SvgComponent = (props) => (
-  <Svg
-    xmlns="http://www.w3.org/2000/svg"
-    width={61}
-    height={61}
-    fill="none"
-    {...props}
-  >
-    <Circle cx={30.024} cy={30.024} r={29.524} fill="#fff" stroke="#000" />
-    <Path
-      stroke="#000"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M30 11v38m19-19H11"
-    />
-  </Svg>
+
+const SvgComponent = ({props}) => (
+  <View style={{ aspectRatio: 1, width: props.width }}>
+    <Svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={'100%'}
+      height={'100%'}
+      fill="none"
+      viewBox="0 0 61 61"
+    >
+      <Circle cx={30.024} cy={30.024} r={29.524} fill="#fff" stroke="#000" />
+      <Path
+        stroke="#000"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M30 11v38m19-19H11"
+      />
+    </Svg>
+  </View>
 )
+
 export { SvgComponent as AddButton }
