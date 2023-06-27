fd15347018f027855dd0941523b11f757c53251f
Alyssa <alyssa@kvasl.com>
2023-06-27 14:03:11 +1200

    Uninstalled bad colourpicker

--- Components/SVGIcons/ColouredCircle.js
+++ Components/SVGIcons/ColouredCircle.js
@@ -1,22 +1,14 @@
-// import { StyleSheet, Text, View } from 'react-native'
-// import React from 'react'
-// import { Slider, Sketch, Material, Colorful, Compact, Circle, Wheel, Block, Github, Chrome } from '@uiw/react-color';
-// import { Alpha, Hue, ShadeSlider, Saturation, Interactive, hsvaToHslaString } from '@uiw/react-color';
-// import { EditableInput, EditableInputRGBA, EditableInputHSLA } from '@uiw/react-color';
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
-// const ColouredCircle = () => {
-//     const [hex, setHex] = useState("#fff");
-//   return (
-//     <Circle
-//       style={{ marginLeft: 20 }}
-//       color={hex}
-//       onChange={(color) => {
-//         setHex(color.hex);
-//       }}
-//     />
-//   );
-// }
 
-// export default ColouredCircle
+const ColouredCircle = () => {
+    const [hex, setHex] = useState("#fff");
+  return (
+<Text>hi</Text>
+  );
+}
 
-// const styles = StyleSheet.create({})
+export default ColouredCircle
+
+const styles = StyleSheet.create({})
