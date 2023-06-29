df783fbc1dca6d884091dbae65f210f321c1a455
Alyssa <alyssa@kvasl.com>
2023-06-29 14:02:02 +1200

    Created component to show a single garden

---
+++ Components/OneGarden.js
@@ -0,0 +1,38 @@
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import H1 from './H1'
+import H2 from './H2'
+import MyText from './MyText'
+import PlantCard from './PlantCard'
+
+import React, { useState, useEffect } from 'react'
+
+//Garden details will be drilled from the garden selector
+const OneGarden = ({props}) => {
+
+    //Will navigate to add a plant page
+    const noPlants = ()=> {
+      return(
+          <View>
+              <MyText>You have no plants in your garden yet!</MyText>
+              <Button title='Add A Plant' />
+          </View>
+      )
+  }
+
+  return (
+    <View>
+      <H1>PlaceholderName{props.garden_name}</H1>
+      <FlatList
+        data={props}
+        renderItem={({item}) => <PlantCard props={item}/>}
+        keyExtractor={(item)=>item.plant_id}
+        numColumns={2}
+        ListEmptyComponent={noPlants}
+       />
+    </View>
+  )
+}
+
+export default OneGarden
+
+const styles = StyleSheet.create({})
