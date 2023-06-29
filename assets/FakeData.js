90f6e1ce361382674a24d7a938e1560a7aa841ed
Alyssa <alyssa@kvasl.com>
2023-06-29 14:16:18 +1200

    Created fake data for testing

---
+++ assets/FakeData.js
@@ -0,0 +1,123 @@
+import colours from '../colours';
+export const GARDENS = [
+    {
+        garden_name:'GardenOne',
+        garden_id: 1,
+        garden_colour: colours.green,
+        garden_colour_light: colours.greenLight,
+        garden_plants: [1, 2, 3, 4]
+    },
+    {
+        garden_name: 'GardenTwo',
+        garden_id: 2,
+        garden_colour: colours.purple,
+        garden_colour_light: colours.purpleLight,
+        garden_plants: [5, 6]
+
+    },
+    {
+        garden_name: 'GardenThree',
+        garden_id: 3,
+        garden_colour: colours.mustard,
+        garden_colour_light: colours.mustardLight,
+        garden_plants: [7, 8]
+
+    },
+    {
+        garden_name: 'GardenFour',
+        garden_id: 4,
+        garden_colour: colours.red,
+        garden_colour_light: colours.redLight,
+        garden_plants: [9, 10]
+
+    },
+    {
+        garden_name: 'GardenFive',
+        garden_id: 5,
+        garden_colour: colours.navy,
+        garden_colour_light: colours.navyLight,
+        garden_plants: [11, 12]
+
+
+    },
+    {
+        garden_name: 'GardenSix',
+        garden_id: 6,
+        garden_colour: colours.red,
+        garden_colour_light: colours.redLight,
+        garden_plants: []
+
+
+    }
+];
+
+//Placeholder
+import lettu from '../assets/Lettuce_StockProduce.png'
+import rhu from '../assets/Rhubarb_StockProduce.png'
+import  her from '../assets/HierloomCarrot_StockProduce.png'
+import art from '../assets/Artichoke_StockProduce.png'
+import cap from '../assets/Capsicum_StockProduce.png'
+import cit from '../assets/Citrus_StockProduce.png'
+
+export const PLANTS = [
+    {
+        plant_id: 1,
+        plant_name:'lettuce',
+        plant_image: lettu
+    },
+    {
+        plant_id: 2,
+        plant_name: 'rhubarb',
+        plant_image: rhu
+    },
+    {
+        plant_id: 3,
+        plant_name: 'heirloomCarrot',
+        plant_image: her
+    },
+    {
+        plant_id: 4,
+        plant_name: 'artichoke',
+        plant_image: art
+    },
+    {
+        plant_id: 5,
+        plant_name: 'capsicum',
+        plant_image: cap
+    },
+    {
+        plant_id: 6,
+        plant_name: 'citrus',
+        plant_image: cit
+    },
+    {
+        plant_id: 7,
+        plant_name:'lettuce',
+        plant_image: lettu
+    },
+    {
+        plant_id: 8,
+        plant_name: 'rhubarb',
+        plant_image: rhu
+    },
+    {
+        plant_id: 9,
+        plant_name: 'heirloomCarrot',
+        plant_image: her
+    },
+    {
+        plant_id: 10,
+        plant_name: 'artichoke',
+        plant_image: art
+    },
+    {
+        plant_id: 11,
+        plant_name: 'capsicum',
+        plant_image: cap
+    },
+    {
+        plant_id: 12,
+        plant_name: 'citrus',
+        plant_image: cit
+    }
+]
