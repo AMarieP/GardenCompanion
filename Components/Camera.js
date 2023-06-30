624e0d573e3a6a6d1113cfdde2cee7e271ab142f
Alyssa <alyssa@kvasl.com>
2023-06-30 12:21:18 +1200

    Styled camera appropriately, added icon buttons

--- Components/Camera.js
+++ Components/Camera.js
@@ -1,6 +1,8 @@
-import { StyleSheet, Image, View, Button } from 'react-native'
+import { StyleSheet, Image, View, Button, Pressable, Text } from 'react-native'
 import React, { useState } from 'react'
 import colours from '../colours'
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 
 //Bug w launchCameraAsync on android 
 import { launchImageLibraryAsync } from 'expo-image-picker'
@@ -16,14 +18,35 @@ const Camera = () => {
 
   return (
     <View>
-      <View style={{width: '100%', height: 300, backgroundColor: colours.greenLight, marginTop: 20}}>
-        <Image source={{uri:pickedImage, height: 300, width: '100%'}} />
+      <View style={{width: '100%', height: '100%', backgroundColor: colours.greenLight}}>
+        <Image source={{uri:pickedImage, height: '100%', width: '100%'}} />
       </View>
-      <Button title='Add Photo From Device' onPress={handleCamera} color={colours.green} />
+      <View style={styles.buttons}>
+          <Pressable onPress={handleCamera} style={styles.button}><Ionicons name="images" size={25} color={colours.greenLight}/></Pressable>
+          <Pressable onPress={handleCamera} style={styles.button}><Ionicons name="camera" size={25} color={colours.greenLight}/></Pressable>
+        </View>
     </View>
   )
 }
 
 export default Camera
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  button:{
+    backgroundColor: colours.green,
+    height: 60,
+    width: 60,
+    margin: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 50,
+  },
+  buttons: {
+    flexDirection: 'row-reverse',
+    position: 'absolute',
+    zIndex: 10,
+    marginTop: '80%',
+    marginLeft: '2%'
+
+  }
+})
